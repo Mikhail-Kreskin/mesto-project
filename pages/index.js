df517c75bdd65dfa22ef0fd5220718b8101c93f1
Mikhail Kreskin <mikhail.kreskin@yandex.ru>
2023-07-27 00:22:52 +0300

    JS file optimization

--- pages/index.js
+++ pages/index.js
@@ -1,100 +1,132 @@
-const container = document.querySelector('.page');
-const editNames_button = document.querySelector('.profile__edit-button');
-const editPictures_button = document.querySelector('.profile__plus-container');
-const popup_opened_namesChanges = document.querySelector('.page__names-changes');
-const popup_opened_picturesChanges = document.querySelector('.page__picture-changes');
 const popupCloseName_button = document.querySelector('.popup__close-button');
-const popupClosePicture_button = document.getElementById('popup__close-button');
-var gridsContainer = document.querySelector('.page__grid-items');
-const profileNameInput = document.getElementById('popup__name');
-const profilePositionInput = document.getElementById('popup__position');
-const pictureNameInput = document.getElementById('name__place');
-const pictureLinkInput = document.getElementById('picture__link');
+
+/* Лайки для карточек выведены в отдельную функцию */
+
+function clickCardsLike(heart) {
+  heart.addEventListener('click', function (evt) {
+    evt.target.classList.toggle('grid-items__heart_type_active');
+  });
+};
+
+/* Удаление карточек выведено в отдельную функцию */
+
+function cardsDelete(trash) {
+  trash.addEventListener('click', function (event) {
+    event.target.closest('.grid-items__item').remove();
+  });
+};
+
+/* Функция открытия попапов */
+
+function openPopups(open_button) {
+  open_button.classList.add("popup_opened");
+};
+
+/* Функция закрытия попапов */
+
+function closePopups(close_button) {
+  close_button.classList.remove("popup_opened");
+};
+
+/* Переменные темплейта вынесены в отдельную функцию */
+let cardsSources = [];
+function createCards(source) {
+  const gridTemplate = source.content;
+  const gridItem = gridTemplate.querySelector('.grid-items__item').cloneNode(true);
+  const gridPopupContainer = gridItem.querySelector('.grid-items__popup-container');
+  return cardsSources = [gridItem, gridPopupContainer];
+};
 
 /* Первоначальная загрузка массива карточек */
 
-  const initialCardsList = [
-    {
-      placeName: "Dream job",
-      link: "https://images.unsplash.com/photo-1625924033080-1e699217e433?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGlubm9wb2xpc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
-      pictureName: "Innopolis.university"
-    },
-    {
-      placeName: "Dream coding",
-      link: "https://images.unsplash.com/photo-1628258334105-2a0b3d6efee1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8ZHJlYW0lMjBjb2RpbmclMjBwbGFjZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
-      pictureName: "Coding place"
-    },
-    {
-      placeName: "Dream chill",
-      link: "https://images.unsplash.com/photo-1516483638261-f4dbaf036963?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGRyZWFtJTIwY2hpbGwlMjBwbGFjZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
-      pictureName: "Chill place"
-    },
-    {
-      placeName: "Dream food",
-      link: "https://images.unsplash.com/photo-1567745218034-06ff90f0e058?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTMwfHxmb29kJTIwcGxhY2V8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
-      pictureName: "Food place"
-    },
-    {
-      placeName: "Dream travel",
-      link: "https://images.unsplash.com/photo-1664462581594-410425f5a1a4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjd8fGRyZWFtJTIwdHJhdmVsfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
-      pictureName: "Travel place"
-    },
-    {
-      placeName: "Dream home",
-      link: "https://images.unsplash.com/photo-1600210491892-03d54c0aaf87?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fGhvbWV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
-      pictureName: "Dream home"
-    },
-  ];
+const initialCardsList = [
+  {
+    placeName: "Dream job",
+    link: "https://images.unsplash.com/photo-1625924033080-1e699217e433?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGlubm9wb2xpc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    pictureName: "Innopolis.university"
+  },
+  {
+    placeName: "Dream coding",
+    link: "https://images.unsplash.com/photo-1628258334105-2a0b3d6efee1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8ZHJlYW0lMjBjb2RpbmclMjBwbGFjZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    pictureName: "Coding place"
+  },
+  {
+    placeName: "Dream chill",
+    link: "https://images.unsplash.com/photo-1516483638261-f4dbaf036963?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGRyZWFtJTIwY2hpbGwlMjBwbGFjZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    pictureName: "Chill place"
+  },
+  {
+    placeName: "Dream food",
+    link: "https://images.unsplash.com/photo-1567745218034-06ff90f0e058?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTMwfHxmb29kJTIwcGxhY2V8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
+    pictureName: "Food place"
+  },
+  {
+    placeName: "Dream travel",
+    link: "https://images.unsplash.com/photo-1664462581594-410425f5a1a4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjd8fGRyZWFtJTIwdHJhdmVsfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+    pictureName: "Travel place"
+  },
+  {
+    placeName: "Dream home",
+    link: "https://images.unsplash.com/photo-1600210491892-03d54c0aaf87?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fGhvbWV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
+    pictureName: "Dream home"
+  },
+];
 
-  initialCardsList.forEach(function (item) {
-    const gridTemplate = document.querySelector('.grid__template').content;
-    const gridItem = gridTemplate.querySelector('.grid-items__item').cloneNode(true);
-    const gridPopupContainer = gridItem.querySelector('.grid-items__popup-container');
-    gridItem.querySelector('.grid-items__place-name').textContent = item.placeName;
-    gridItem.querySelector('.grid-items__img').src = item.link;
-    gridItem.querySelector('.grid-items__img').alt = item.pictureName;
-    const gridImage = gridItem.querySelector('.grid-items__img');
-    gridImage.addEventListener('click', function () {
-      gridPopupContainer.classList.add("popup_opened");
-      gridItem.querySelector('.grid-items__popup-title').textContent = gridItem.querySelector('.grid-items__place-name').textContent;
-      gridItem.querySelector('.grid-items__popup-img').src = gridImage.src;
-      gridItem.querySelector('.grid-items__popup-img').alt = gridImage.alt;
-    });
-    gridItem.querySelector('.grid-items__close-button').addEventListener('click', function () {
-      gridPopupContainer.classList.remove("popup_opened");
-    });
-    gridsContainer.append(gridItem);
-
-
-    gridItem.querySelector('.grid-items__heart').addEventListener('click', function (evt) {
-      evt.target.classList.toggle('grid-items__heart_type_active');
-    });
-
-    gridItem.querySelector('.grid-items__trash').addEventListener('click', function (event) {
-      event.target.closest('.grid-items__item').remove();
-    });
+var gridsContainer = document.querySelector('.page__grid-items');
+initialCardsList.forEach(function (item) {
+  createCards(document.querySelector('.grid__template'));
+  cardsSources[0].querySelector('.grid-items__place-name').textContent = item.placeName;
+  cardsSources[0].querySelector('.grid-items__img').src = item.link;
+  cardsSources[0].querySelector('.grid-items__img').alt = item.pictureName;
+  const gridImage = cardsSources[0].querySelector('.grid-items__img');
+  gridImage.addEventListener('click', function () {
+    openPopups(cardsSources[1]);
+    cardsSources[0].querySelector('.grid-items__popup-title').textContent = item.placeName;
+    cardsSources[0].querySelector('.grid-items__popup-img').src = gridImage.src;
+    cardsSources[0].querySelector('.grid-items__popup-img').alt = gridImage.alt;
   });
+  cardsSources[0].querySelector('.grid-items__close-button').addEventListener('click', function () {
+    closePopups(cardsSources[1]);
+  });
+  gridsContainer.append(cardsSources[0]);
+
+  clickCardsLike(cardsSources[0].querySelector('.grid-items__heart'));
+
+  cardsDelete(cardsSources[0].querySelector('.grid-items__trash'));
+
+});
 
 /* Открытие и закрытие попапа для редактирования имени и позиции */
+
+const editNames_button = document.querySelector('.profile__edit-button');
+const popup_opened_namesChanges = document.querySelector('.page__names-changes');
+const profileNameInput = document.getElementById('popup__name');
+const profilePositionInput = document.getElementById('popup__position');
+
 editNames_button.addEventListener('click', function () {
-  popup_opened_namesChanges.classList.add("popup_opened");
+  openPopups(popup_opened_namesChanges);
   profileNameInput.value = "";
   profilePositionInput.value = "";
 });
-
 popupCloseName_button.addEventListener('click', function () {
-  popup_opened_namesChanges.classList.remove("popup_opened");
+  closePopups(popup_opened_namesChanges);
 });
 
 /* Открытие и закрытие попапа для редактирования картинки и названия*/
+
+const editPictures_button = document.querySelector('.profile__plus-container');
+const popup_opened_picturesChanges = document.querySelector('.page__picture-changes');
+const pictureNameInput = document.getElementById('name__place');
+const pictureLinkInput = document.getElementById('picture__link');
+const popupClosePicture_button = document.getElementById('popup__close-button');
 editPictures_button.addEventListener('click', function () {
-  popup_opened_picturesChanges.classList.add("popup_opened");
+  openPopups(popup_opened_picturesChanges);
   pictureNameInput.value = "";
   pictureLinkInput.value = "";
 });
 
 popupClosePicture_button.addEventListener('click', function () {
-  popup_opened_picturesChanges.classList.remove("popup_opened");
+  closePopups(popup_opened_picturesChanges);
 });
 
 /*Замена имени и позиции на текст из попапа*/
@@ -103,14 +135,14 @@ const popupSaveButton = document.getElementById('popup__saveButton');
 
 popupSaveButton.addEventListener('click', function () {
   if (profileNameInput.value === '' || profilePositionInput.value === '') {
-    alert("Необходимо ввести имя или должность")
+    alert("Необходимо ввести имя и должность")
   }
   else {
     document.querySelector('.profile__name').textContent = "";
     document.querySelector('.profile__description').textContent = "";
     document.querySelector('.profile__name').textContent = profileNameInput.value;
     document.querySelector('.profile__description').textContent = profilePositionInput.value;
-    popup_opened_namesChanges.classList.remove("popup_opened");
+    closePopups(popup_opened_namesChanges);
   }
 });
 
@@ -120,34 +152,27 @@ const popupCreateButton = document.getElementById('popup__createButton');
 
 popupCreateButton.addEventListener('click', function () {
   if (pictureNameInput.value === '' || pictureLinkInput.value === '') {
-    alert("Необходимо ввести название места или ссылку на изображение")
+    alert("Необходимо ввести название места и ссылку на изображение")
   }
   else {
-    const gridTemplate = document.querySelector('.grid__template').content;
-    const gridItem = gridTemplate.querySelector('.grid-items__item').cloneNode(true);
-    const gridPopupContainer = gridItem.querySelector('.grid-items__popup-container');
-    gridItem.querySelector('.grid-items__place-name').textContent = pictureNameInput.value;
-    const gridImage = gridItem.querySelector('.grid-items__img');
+    createCards(document.querySelector('.grid__template'));
+    cardsSources[0].querySelector('.grid-items__place-name').textContent = pictureNameInput.value;
+    const gridImage = cardsSources[0].querySelector('.grid-items__img');
     gridImage.src = pictureLinkInput.value;
     gridImage.alt = pictureNameInput.value;
-    gridsContainer.prepend(gridItem);
-
-    gridItem.querySelector('.grid-items__heart').addEventListener('click', function (evt) {
-      evt.target.classList.toggle('grid-items__heart_type_active');
-    });
-    gridItem.querySelector('.grid-items__trash').addEventListener('click', function (event) {
-      event.target.closest('.grid-items__item').remove();
-    });
-
+    gridsContainer.prepend(cardsSources[0]);
+    clickCardsLike(cardsSources[0].querySelector('.grid-items__heart'));
+    cardsDelete(cardsSources[0].querySelector('.grid-items__trash'));
     gridImage.addEventListener('click', function () {
-      gridPopupContainer.classList.add("popup_opened");
-      gridItem.querySelector('.grid-items__popup-title').textContent = gridItem.querySelector('.grid-items__place-name').textContent;
-      gridItem.querySelector('.grid-items__popup-img').src = gridImage.src;
-      gridItem.querySelector('.grid-items__popup-img').alt = gridImage.alt;
+      openPopups(cardsSources[1]);
+      cardsSources[0].querySelector('.grid-items__popup-title').textContent = cardsSources[0].querySelector('.grid-items__place-name').textContent;
+      cardsSources[0].querySelector('.grid-items__popup-img').src = gridImage.src;
+      cardsSources[0].querySelector('.grid-items__popup-img').alt = gridImage.alt;
     });
-    gridItem.querySelector('.grid-items__close-button').addEventListener('click', function () {
-      gridPopupContainer.classList.remove("popup_opened");
+    cardsSources[0].querySelector('.grid-items__close-button').addEventListener('click', function () {
+      closePopups(cardsSources[1]);
     });
-    popup_opened_picturesChanges.classList.remove("popup_opened");
+    closePopups(popup_opened_picturesChanges);
   }
 });
+
